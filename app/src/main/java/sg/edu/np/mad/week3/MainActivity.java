6f8ac79f44943289a82098521cf18a244306b5fb
BuildTools <unconfigured@null.spigotmc.org>
2020-05-06 20:21:41 +0800

    Week 3 Practical Done

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,11 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("MAD Wk3");
+
+        ImageView cuteBabyImage = findViewById(R.id.imageView2);
+        cuteBabyImage.setImageURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.cute_baby)
+        );
+
+        MediaPlayer babyLaughingAudio = MediaPlayer.create(this, R.raw.baby_laughing);
+        babyLaughingAudio.start();
+
+        VideoView rabbidVideo = findViewById(R.id.videoView);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid)
+        );
+        rabbidVideo.start();
     }
 }
